5db7382eb444935c03ef6bd64fdaf0eb18fc4d7d
Giga <gigakatamadze0@gmail.com>
2025-06-10 16:40:01 +0400

    Initial commit: Spring Boot project structure with .gitignore

---
+++ src/test/java/com/notificationsystem/notificationfacade/NotificationFacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notificationsystem.notificationfacade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationFacadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
